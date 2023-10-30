01e9e1d7807cad8fe40b8469d9edaf4e96632bcd
meunrud <113085963+meunrud@users.noreply.github.com>
2023-10-30 11:52:37 +0900

    feat : 유저 입력 받는 메서드에 async/await 추가 refactor : outputGameMessage() -> printGameMessage() 로 변경

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { Console } from '@woowacourse/mission-utils';
 
+const INPUT_CAR_NAME =
+  '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)';
+
 class App {
   async play() {
-    this.outputGameMessage();
+    this.printGameMessage();
   }
 
-  outputGameMessage() {
-    const inputCarname =
-      '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)';
-    Console.print(inputCarname);
+  async printGameMessage() {
+    const playerInput = await Console.readLineAsync(INPUT_CAR_NAME);
+    return playerInput;
   }
 }
 
