a0527b5182f36796cbfffaae07cffc5abe12e7ba
meunrud <113085963+meunrud@users.noreply.github.com>
2023-10-30 15:10:06 +0900

    테스트 통과

--- src/App.js
+++ src/App.js
@@ -1,52 +1,70 @@
 import { Console, MissionUtils } from '@woowacourse/mission-utils';
-import GAME_MESSAGE from './constants/gameMessage';
-
-
+import GAME_MESSAGE from './constants/gameMessage.js';
+import Car from './car.js';
+import Game from './game.js';
 
 class App {
-
   async play() {
+    const playerNames = await this.printGameMessage();
+    const times = await this.printNumberOfTimes();
 
+    const cars = playerNames.map((name) => new Car(name));
+    const racingGame = new Game(cars);
+    Console.print('실행 결과');
+
+    racingGame.race(times);
+
+    const winners = racingGame.getWinners();
+    Console.print(winners);
+  }
+
+  async printNumberOfTimes() {
+    const playerInput = await Console.readLineAsync(
+      GAME_MESSAGE.INPUT_NUMBER_OF_TIMES,
+    );
+    const numberOfTimes = playerInput.split('').map(Number);
+    return numberOfTimes[0];
   }
 
   async printGameMessage() {
-    const playerInput = await Console.readLineAsync(GAME_MESSAGE.INPUT_CAR_NAME);
-    return playerInput.split(",").map(name => name.trim());
+    const playerInput = await Console.readLineAsync(
+      GAME_MESSAGE.INPUT_CAR_NAME,
+    );
+    const playerCarName = this.toArray(playerInput);
+    return playerCarName;
   }
 
-  async printNumberOfTimes(){
-    const playerInput = await Console.readLineAsync(GAME_MESSAGE.INPUT_NUMBER_OF_TIMES);
-    return playerInput.map(Number);
+  toArray(input) {
+    this.input = input;
+    if (!this.includeSemiColon(input)) {
+      throw new Error('[ERROR] 이름은 쉼표로 구분되어야 합니다.');
+    }
+    const names = input.split(',').map((name) => name.trim());
+    if (!this.isValidNameFormat(names)) {
+      throw new Error('[ERROR] 이름은 5자를 넘을 수 없습니다.');
+    }
+    return names;
   }
 
   belowThanFive(carName) {
-    return carName.length <= 5;
-  }
-  
-  isValidPlayerCarName(playerInput) {
-    return playerInput.every(name => this.belowThanFive(name));
-
-    }
- //게임 진행
-    generateRandomNumbers(){
-      return MissionUtils.Random.pickNumber(0,9);
-    }
-    
-    moreThanFour(randomNumber){
-      return randomNumber >= 4
+    this.carName = [];
+    for (let i = 0; i < carName.length; i++) {
+      if (carName[i].length > 5) {
+        throw new Error('[ERROR] 이름은 5자를 넘을 수 없습니다.  ');
+      }
     }
   }
-  moveBy(){
-    const randomNumber = this.generateRandomNumbers();
-  if(this.moreThanFour(randomNumber)){
-    this.position +=1;
-  }  }
 
- 
+  isValidNameFormat(names) {
+    return names.every((name) => name.length <= 5);
+  }
 
-
- 
-
+  includeSemiColon(input) {
+    return input.includes(',');
+  }
 }
+
+const app = new App();
+app.play();
 
 export default App;
